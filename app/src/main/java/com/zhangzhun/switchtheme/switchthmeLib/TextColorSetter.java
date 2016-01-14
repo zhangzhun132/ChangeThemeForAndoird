4f5e4684edce8a53728c7a819f087c2b465819cc
zhangzhun132 <15201246675@163.com>
2016-01-14 10:47:43 +0800

    为Android应用设置主题，而不用重新运行Activity，例如可以无闪屏的切换日间、夜间模式，类似于知乎App。

---
+++ app/src/main/java/com/zhangzhun/switchtheme/switchthmeLib/TextColorSetter.java
@@ -0,0 +1,28 @@
+package com.zhangzhun.switchtheme.switchthmeLib;
+
+import android.content.res.Resources.Theme;
+import android.widget.TextView;
+/**
+ * TextView 文本颜色Setter
+ * @author mrsimple
+ *
+ */
+public class TextColorSetter extends ViewSetter {
+
+	public TextColorSetter(TextView textView, int resId) {
+		super(textView, resId);
+	}
+
+	public TextColorSetter(int viewId, int resId) {
+		super(viewId, resId);
+	}
+
+	@Override
+	public void setValue(Theme newTheme, int themeId) {
+		if (mView == null) {
+			return;
+		}
+		((TextView) mView).setTextColor(getColor(newTheme));
+	}
+
+}
